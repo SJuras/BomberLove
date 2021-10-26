65c57d5594f62ffc08db2cf2f8ae4e756da2b63e
SJuras <jurasstpko@gmailcom>
2021-10-26 22:29:22 +0200

    v1.1.3 finishing the hero section of the home page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// toggle menu
+function toggleMenu(){
+  const sidemenu = document.querySelector(".sidemenu");
+  const toggle = document.querySelector(".toggle");
+  sidemenu.classList.toggle("active");
+  toggle.classList.toggle("active");
+}
+// toggle menu END
