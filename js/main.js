9b0e3cda32fd251889f32bc2c0a5151c7b1d6b25
SJuras <jurasstpko@gmailcom>
2021-10-27 03:59:47 +0200

    v1.1.5 added scroll to top button

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ function toggleMenu(){
   toggle.classList.toggle("active");
 }
 // toggle menu END
+
+// scroll up button
+const btnScrollTop = document.querySelector('#btnScrollTop');
+window.addEventListener("scroll", function(){
+  btnScrollTop.classList.toggle("visible", window.scrollY > 100);
+});
+
+btnScrollTop.addEventListener("click", function(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+// scroll up button END 
